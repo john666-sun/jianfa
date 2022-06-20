af84c74a6fc6a06adf6072ff77de21efddb5fee8
sen <sen@atguigu.com>
2022-06-20 22:46:43 +0800

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName：test
+ * @Author: xxx
+ * @Date: 2022/6/20 22:44
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
